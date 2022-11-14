7179f9a3308d40f456fa0f173c5a9ac825013c31
Robert Manders <31056731+Rob-Manders@users.noreply.github.com>
2022-11-14 19:10:27 +0000

    New list router.

--- components/CreateList/CreateList.tsx
+++ components/CreateList/CreateList.tsx
@@ -3,12 +3,14 @@ import { useContext, useState } from 'react'
 import useValidInput from '../../hooks/useValidInput'
 import { UserContext } from '../../context/UserContext'
 import { addDoc, collection, getFirestore } from 'firebase/firestore'
+import { useRouter } from 'next/router'
 
 export default function CreateList() {
 	const [inputValue, setInputValue] = useState('')
 	const [valid, setValid] = useState(false)
 	const { validInput } = useValidInput()
 	const { user } = useContext(UserContext)
+	const router = useRouter()
 
 	function onChange(event) {
 		setInputValue(event.target.value)
@@ -36,7 +38,8 @@ export default function CreateList() {
 				listItems: {}
 			})
 
-			console.log(newList)
+			// setInputValue('')
+			router.push(`/list/${newList.id}`)
 		}
 	}
 
