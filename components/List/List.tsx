997e84805b4c555a46b2cb8c827743ed32d64a0e
Robert Manders <31056731+Rob-Manders@users.noreply.github.com>
2022-11-15 18:36:23 +0000

    List completion status.

--- components/List/List.tsx
+++ components/List/List.tsx
@@ -1,11 +1,30 @@
 import styles from './List.module.scss'
 import Link from 'next/link'
-import { doc, deleteDoc, getFirestore } from 'firebase/firestore'
-import { useContext } from 'react'
+import { doc, deleteDoc, getFirestore, collection } from 'firebase/firestore'
+import { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../../context/UserContext'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
 
 export default function List({ id, list }) {
 	const { user } = useContext(UserContext)
+	const [complete, setComplete] = useState(false)
+	const [values] = useCollectionData(
+		collection(getFirestore(), 'users', user.uid, 'lists', id, 'items')
+	)
+
+	useEffect(() => {
+		let incompleteItems
+
+		if (values) {
+			incompleteItems = values.reduce((incomplete, item) => {
+				if (item.complete) return incomplete++
+			}, 0)
+		}
+
+		if (incompleteItems === 0 && values.length > 0) {
+			setComplete(true)
+		}
+	}, [values])
 
 	async function deleteList() {
 		await deleteDoc(doc(getFirestore(), 'users', user.uid, 'lists', id))
@@ -13,6 +32,7 @@ export default function List({ id, list }) {
 
 	return (
 		<div className={styles.list}>
+			{complete ? 'Y ' : 'N '}
 			<Link href={`/${user.uid}/${id}`}>{list.listName}</Link>
 			<button onClick={() => deleteList()}>X</button>
 		</div>
